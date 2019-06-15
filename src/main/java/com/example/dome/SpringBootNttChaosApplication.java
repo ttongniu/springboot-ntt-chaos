3a05981800d0f54b7ae2dfb3361cf6dd711b241a
ttongniu <ttongniu@163.com>
2019-06-15 19:11:50 +0800

    Initial commit

---
+++ src/main/java/com/example/dome/SpringBootNttChaosApplication.java
@@ -0,0 +1,13 @@
+package com.example.dome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNttChaosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootNttChaosApplication.class, args);
+  }
+
+}
